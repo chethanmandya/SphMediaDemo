d3751de92463f4e913cd8cfa1c63fa40c295feec
Chethan <chethu002@gmail.com>
2024-10-28 04:12:26 +0530

    some more improvement

--- domain/src/main/java/com/sphmedia/domain/repository/BreweryRepository.kt
+++ domain/src/main/java/com/sphmedia/domain/repository/BreweryRepository.kt
@@ -1,13 +1,9 @@
 package com.sphmedia.domain.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import com.sphmedia.data.api.BreweryService
 import com.sphmedia.data.db.BreweryCacheDao
 import com.sphmedia.data.db.BreweryDao
 import com.sphmedia.data.model.Brewery
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class BreweryRepository @Inject constructor(
@@ -15,32 +11,6 @@ class BreweryRepository @Inject constructor(
     private val breweryApi: BreweryService, // Retrofit API Service
     private val breweryCacheDao: BreweryCacheDao
 ) {
-//    fun getBreweries(page: Int, perPage: Int): Flow<Resource<List<Brewery>>> {
-//        return object : NetworkBoundResource<List<Brewery>, List<Brewery>>() {
-//
-//            // Load data from Room
-//            override fun loadFromDb(): Flow<List<Brewery>> {
-//                return breweryDao.getBreweryPage(page, perPage)
-//            }
-//
-//            // Fetch data from API for the current page
-//            override suspend fun fetchFromNetwork(): List<Brewery> {
-//                return apiService.getBreweriesByType("micro", perPage, page)
-//            }
-//
-//            // Save API response to the Room database
-//            override suspend fun saveNetworkResult(item: List<Brewery>) {
-//                breweryDao.insertAll(item)
-//            }
-//
-//            // Optionally, decide whether to fetch new data
-//            override fun shouldFetch(data: List<Brewery>?): Boolean {
-//                return data.isNullOrEmpty() || data.size < perPage
-//            }
-//
-//        }.asFlow()
-//    }
-//} ̰
 
 
     // Modify this method to accept the page number
